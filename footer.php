d647abb94d1c9c46765b23728adce36acfa74eb2
Yudha H Tejaningrat <yudha_ht@yahoo.com>
2013-10-26 15:59:26 +0000

    change: header, nav-strip, footer

--- footer.php
+++ footer.php
@@ -1,28 +1,28 @@
-<?php
-/**
- *	@package WordPress
- *	@subpackage kisi-kisi-hitam
- */
-?>
-	<div id="footer" class="fix">
-		<p class="left"><a href="http://blog.yht.web.id/tag/kisi-kisi-hitam" title="kisi-kisi-hitam : Yudha_HT">kisi-kisi-hitam</a> oleh Yudha_HT [<a href="http://yht.web.id">yht.web.id</a>].</p>
-		<p class="right">&nbsp;</p>
-	</div>
-</div>
-<?php wp_footer(); ?>
-</body>
+   <div id="footer" class="fix">
+    <p class="left"><a href="//github.com/yht/kisi-kisi-hitam" title="kisi-kisi-hitam">kisi-kisi-hitam</a> oleh Yudha H Tejaningrat.</p>
+    <p class="right">&nbsp;</p>
+   </div>
+  </div>
+  <?php wp_footer(); ?>
+ </body>
 
 <!-- Piwik -->
 <script type="text/javascript">
-var pkBaseURL = (("https:" == document.location.protocol) ? "https://analisis.b2b.web.id/" : "http://analisis.b2b.web.id/");
-document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
-</script><script type="text/javascript">
-try {
-var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 1);
-piwikTracker.trackPageView();
-piwikTracker.enableLinkTracking();
-} catch( err ) {}
-</script><noscript><p><img src="http://analisis.b2b.web.id/piwik.php?idsite=1" style="border:0" alt="" /></p></noscript>
-<!-- End Piwik Tag -->
+  var _paq = _paq || [];
+  _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+  _paq.push(["setCookieDomain", "*.yht.web.id"]);
+  _paq.push(["setDomains", ["*.yht.web.id","*.blog.yht.web.id"]]);
+  _paq.push(["trackPageView"]);
+  _paq.push(["enableLinkTracking"]);
+
+  (function() {
+    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://piwik.kalamangga.net/";
+    _paq.push(["setTrackerUrl", u+"piwik.php"]);
+    _paq.push(["setSiteId", "2"]);
+    var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
+    g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Piwik Code -->
 
 </html>
--- header.php
+++ header.php
@@ -1,34 +1,39 @@
-<?php
-/**
- *	@package WordPress
- *	@subpackage Grid_Focus
- */
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
+<html lang="id" dir="ltr" itemscope itemtype="http://schema.org/Article">
 
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head profile="http://gmpg.org/xfn/11">
-	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-	<title><?php if (function_exists('is_tag') && is_tag()) { echo 'Posts tagged &quot;'.$tag.'&quot; - '; } elseif (is_archive()) { wp_title(''); echo ' Archive - '; } elseif (is_search()) { echo 'Search for &quot;'.wp_specialchars($s).'&quot; - '; } elseif (!(is_404()) && (is_single()) || (is_page())) { wp_title(''); echo ' - '; } elseif (is_404()) { echo 'Not Found - '; } bloginfo('name'); ?></title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-	<meta name="generator" content="WordPress <?php bloginfo('version'); ?>" />
-	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
-	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
-	<script src="<?php bloginfo('template_url') ?>/js/jquery-1.2.6.min.js" type="text/javascript" charset="utf-8"></script>
-	<script src="<?php bloginfo('template_url') ?>/js/functions.js" type="text/javascript" charset="utf-8"></script>
-	<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
-	<?php wp_head(); ?>
-</head>
-<body>
-<div id="wrapper">
-	
-	<div id="masthead" class="fix">
-		<h1><a href="<?php echo get_settings('home'); ?>/"><?php bloginfo('name'); ?></a></h1>
-		<div id="blogLead">
-			<img src="<?php bloginfo('template_directory'); ?>/images/avatar.png" alt="Icon" />
-			<p id="authorIntro"><?php bloginfo('description'); ?></p>
-		</div>
-	</div>
-	
-	<?php include (TEMPLATEPATH . '/navigation.strip.php'); ?>
+ <head>
+
+  <link rel="dns-prefecth" href="//yht.web.id" />
+  <link rel="dns-prefecth" href="//blog.yht.web.id" />
+  <link rel="dns-prefecth" href="//kalamangga.net" />
+  <link rel="dns-prefecth" href="//cdn.kalamangga.net" />
+  <link rel="dns-prefecth" href="//piwik.kalamangga.net" />
+
+  <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+  <title><?php if (function_exists('is_tag') && is_tag()) { echo 'Posts tagged &quot;'.$tag.'&quot; - '; } elseif (is_archive()) { wp_title(''); echo ' Archive - '; } elseif (is_search()) { echo 'Search for &quot;'.wp_specialchars($s).'&quot; - '; } elseif (!(is_404()) && (is_single()) || (is_page())) { wp_title(''); echo ' - '; } elseif (is_404()) { echo 'Not Found - '; } bloginfo('name'); ?></title>
+  <meta name="generator" content="gedit 3.4.2 & nano 2.2.6" />
+  <meta name="author" content="Yudha H Tejaningrat" />
+
+  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+  <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
+
+  <script src="//cdn.kalamangga.net/pustaka/jquery/jquery-1.10.2.min.js" type="text/javascript" charset="utf-8"></script>
+  <script src="//cdn.kalamangga.net/pustaka/jquery/jquery-migrate-1.2.1.min.js" type="text/javascript" charset="utf-8"></script>
+  <script src="<?php bloginfo('template_url') ?>/js/functions.js" type="text/javascript" charset="utf-8"></script>
+
+  <?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
+  <?php wp_head(); ?>
+
+ </head>
+
+ <body>
+  <div id="wrapper">
+   <div id="masthead" class="fix">
+    <h1><a href="<?php echo get_settings('home'); ?>/"><?php bloginfo('name'); ?></a></h1>
+    <div id="blogLead">
+     <img src="<?php bloginfo('template_directory'); ?>/images/avatar.png" alt="Icon" />
+     <p id="authorIntro"><?php bloginfo('description'); ?></p>
+    </div>
+   </div>
+
+<?php include (TEMPLATEPATH . '/navigation.strip.php'); ?>
